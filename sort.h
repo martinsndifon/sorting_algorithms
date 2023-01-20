5c404a08a4adea4921463fcfe15a31adbafde4df
Martins Ndifon <martinsndifon@gmail.com>
2023-01-20 15:46:26 +0000

    update to task 3-quick_sort

--- sort.h
+++ sort.h
@@ -22,5 +22,8 @@ void print_list(const listint_t *list);
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
+int partition(int *arr, int low, int high, size_t size);
+void qs(int *arr, int low, int high, size_t size);
 
 #endif
