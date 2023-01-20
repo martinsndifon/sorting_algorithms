21ceb305cff44a8090e696bf8c9bed920b2c7a1a
Martins Ndifon <martinsndifon@gmail.com>
2023-01-20 11:38:17 +0000

    update to task 0-bubble_sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -10,7 +10,10 @@
 void bubble_sort(int *array, size_t size)
 {
 	size_t i, j;
-	int flag, tmp = 0;
+	int flag, tmp;
+
+	if (size < 2)
+		return;
 
 	for (i = 0; i < (size - 1); i++)
 	{
@@ -29,6 +32,6 @@ void bubble_sort(int *array, size_t size)
 			}
 		}
 		if (flag == 0)
-			break;
+			return;
 	}
 }
