201d0a783cb5031422eb21ba175d4945f7b76391
Martins Ndifon <martinsndifon@gmail.com>
2023-01-22 08:48:06 +0000

    update to advance task 105-radix_sort

--- sort.h
+++ sort.h
@@ -30,5 +30,7 @@ void cocktail_sort_list(listint_t **list);
 void counting_sort(int *array, size_t size);
 void merge_sort(int *array, size_t size);
 void heap_sort(int *array, size_t size);
+void radix_sort(int *array, size_t size);
+void counting_sort_r(int *array, size_t size, int pos, int *out, int *ca);
 
 #endif
