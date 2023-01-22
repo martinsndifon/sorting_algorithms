591cbc22329a9a042d5209b4bbb63a11aa0e9853
Martins Ndifon <martinsndifon@gmail.com>
2023-01-22 08:21:08 +0000

    Update to advance task 101-cocktail_sort_list

--- sort.h
+++ sort.h
@@ -26,5 +26,6 @@ void quick_sort(int *array, size_t size);
 int partition(int *arr, int low, int high, size_t size);
 void qs(int *arr, int low, int high, size_t size);
 void shell_sort(int *array, size_t size);
+void cocktail_sort_list(listint_t **list);
 
 #endif
